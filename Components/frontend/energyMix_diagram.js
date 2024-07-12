810373726ae208f2994c556008f5970d3b9426c2
Luis Wagner <l.wagner@netze-bw.de>
2024-07-12 17:01:11 +0200

    export the energyMix // file for the diagram

---
+++ Components/frontend/energyMix_diagram.js
@@ -0,0 +1,2 @@
+//ToDo diagram for energy mix
+//get the data from the API
--- backend/API_GET_energyMix.js
+++ backend/API_GET_energyMix.js
@@ -6,15 +6,18 @@ const { fetchMarketData } = require('./API_GET_market.js');
 fetchMarketData()
   .then(data => {
     const energyMix = data.energy_mix; // Extract only the energy mix
-    const generationTypes = {}; // Object to hold generation types and their power
+    const generationPower = {}; // Object to hold generation types and their power
 
     energyMix.forEach(energy => {
       const { generation_type, total_installed_power_in_watt } = energy;
-      generationTypes[generation_type] = total_installed_power_in_watt;
+      generationPower[generation_type] = total_installed_power_in_watt;
     });
 
-    console.log(generationTypes); // Log the object containing generation types and their power
+    console.log(generationPower); // Log the object containing generation types and their power
   })
   .catch(error => {
     console.error('Error fetching market data:', error);
   });
+
+// Export Energy Mix
+module.exports = generationPower;
